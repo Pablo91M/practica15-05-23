e362b0367bd1881184d88f9399a514ccaebad6b9
Pablo91M <pabloruben12@gmail.com>
2023-05-15 12:32:18 -0300

    mande express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hola mundo!!!')
+const express = require('express') //requiero la libreria
+const PORT = 3001 //defino el puerto
+
+const app = express()
+app.get("/",(req,res)=>{
+    res.status(200).json({"mensaje": "hola mundo!"})
+})
+
+app.listen(PORT, ()=>{console.log(`escuchando en el puerto ${PORT}`)})
